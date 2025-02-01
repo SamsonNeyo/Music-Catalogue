d8fdaa604ae8b7e2335ff2d15c779eab5ddb8c71
samuelemmanuel211 <samuelkhamis06@gmail.com>
2025-02-01 04:45:08 -0800

    prepareheader
    Test passed

--- src/test/java/Music_CataloguePackage/MusicCatalogueTest.java
+++ src/test/java/Music_CataloguePackage/MusicCatalogueTest.java
@@ -23,7 +23,17 @@ class MusicCatalogueTest {
         assertEquals(new Dimension(800, 500), frame.getSize(), "Frame should have correct size");
         assertEquals(BorderLayout.class, frame.getLayout().getClass(), "Frame should use BorderLayout");
     }
+    @Test
+    void testPrepareHeader() {
+        JFrame frame = musicCatalogue.prepareHeader();
+        assertNotNull(frame, "Frame should not be null");
+        JPanel topPanel = (JPanel) frame.getContentPane().getComponent(0);
+        assertNotNull(topPanel, "Top panel should not be null");
+        assertEquals(Color.LIGHT_GRAY, topPanel.getBackground(), "Top panel should have the correct background color");
+        JLabel titleLabel = (JLabel) topPanel.getComponent(0);
+        assertEquals("My Music Catalogue", titleLabel.getText(), "Title label should have correct text");
+    }
 
 
-    
+
 }
