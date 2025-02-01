86d8a0023a798c09bd6d274c6e053f12176881d3
samuelemmanuel211 <samuelkhamis06@gmail.com>
2025-02-01 04:49:34 -0800

    testPrepareSidePanel
    side panel test is successfully tested.
    and the result is passed

--- src/test/java/Music_CataloguePackage/MusicCatalogueTest.java
+++ src/test/java/Music_CataloguePackage/MusicCatalogueTest.java
@@ -33,7 +33,26 @@ class MusicCatalogueTest {
         JLabel titleLabel = (JLabel) topPanel.getComponent(0);
         assertEquals("My Music Catalogue", titleLabel.getText(), "Title label should have correct text");
     }
+    @Test
+    void testPrepareSidePanel() {
+        JFrame frame = musicCatalogue.prepareSidePanel();
+        assertNotNull(frame, "Frame should not be null");
+        JPanel sidePanel = (JPanel) frame.getContentPane().getComponent(1);
+        assertNotNull(sidePanel, "Side panel should not be null");
+        assertEquals(new Color(40, 55, 70), sidePanel.getBackground(), "Side panel should have the correct background color");
+
+        Component[] components = sidePanel.getComponents();
+        String[] expectedCategories = {"Reggae", "Ballads", "Rap"};
+        assertEquals(expectedCategories.length, components.length, "Side panel should contain the correct number of buttons");
+
+        for (int i = 0; i < components.length; i++) {
+            assertTrue(components[i] instanceof JButton, "Side panel should contain buttons");
+            JButton button = (JButton) components[i];
+            assertEquals(expectedCategories[i], button.getText(), "Button should have the correct label");
+            assertEquals(Color.LIGHT_GRAY, button.getBackground(), "Buttons should have the correct background color");
+            assertEquals(Color.DARK_GRAY, button.getForeground(), "Buttons should have the correct text color");
+        }
 
 
-
+    }
 }
