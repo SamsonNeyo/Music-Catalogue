ec4cf77d5f04d4e4bfb86e6d90fb621f3d92a40c
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-01 08:55:51 +0300

    Side Panel
    Side Panel has be added to the sidePanel branch

--- src/main/java/Music_CataloguePackage/MusicCatalogue.java
+++ src/main/java/Music_CataloguePackage/MusicCatalogue.java
@@ -5,20 +5,14 @@ import java.awt.*;
 
 public class MusicCatalogue {
     JFrame mainFrame;
-    JPanel sidePanel, bottomPanel;
-    String[] categories = {"Reggae", "Ballads", "Rap"};
-    JButton submitButton, exitButton;
-
     public MusicCatalogue() {
         this.prepareLayoutBorder();
         this.prepareHeader();
-        this.prepareSidePanel();
-        this.prepareBottomPanel();
     }
     public JFrame prepareLayoutBorder() {
         mainFrame = new JFrame("Music Catalogue");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(800, 600);
+        mainFrame.setSize(800, 500);
         mainFrame.setLayout(new BorderLayout());
 
         mainFrame.setVisible(true);
@@ -30,8 +24,7 @@ public class MusicCatalogue {
     public JFrame prepareHeader() {
         // Top Panel (NORTH) - Title
         JPanel topPanel = new JPanel();
-        topPanel.setBackground(new Color(217,217,217));
-        topPanel.setPreferredSize(new Dimension(800, 50));
+        topPanel.setBackground(Color.LIGHT_GRAY);
         JLabel titleLabel = new JLabel("My Music Catalogue");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
         topPanel.add(titleLabel);
@@ -39,41 +32,4 @@ public class MusicCatalogue {
 
         return  mainFrame;
     }
-
-    public JFrame prepareSidePanel(){
-        sidePanel = new JPanel(new GridLayout(3, 1, 10, 10));
-        sidePanel.setBackground(new Color(255, 255, 233));
-        sidePanel.setPreferredSize(new Dimension(100, 5));
-        for (String category : categories) {
-            JButton btn = new JButton(category);
-            btn.setBackground(new Color(10, 50, 80));
-            btn.setForeground(Color.WHITE);
-            sidePanel.add(btn);
-        }
-        mainFrame.add(sidePanel, BorderLayout.WEST);
-
-        return mainFrame;
-    }
-    public JFrame prepareBottomPanel() {
-        bottomPanel = new JPanel();
-        bottomPanel.setBackground(new Color(217,  217,217));
-        submitButton = new JButton("Submit");
-        exitButton = new JButton("Exit");
-        bottomPanel.setPreferredSize(new Dimension(800, 50));
-        submitButton.setBackground(new Color(3, 60, 91));
-        submitButton.setForeground(Color.WHITE);
-        exitButton.setBackground(new Color(3, 60, 91));
-        // Styling Buttons
-        Font buttonFont = new Font("Arial", Font.BOLD, 16);
-        submitButton.setFont(buttonFont);
-        exitButton.setFont(buttonFont);
-
-        exitButton.setForeground(Color.WHITE);
-
-        bottomPanel.add(submitButton);
-        bottomPanel.add(exitButton);
-        mainFrame.add(bottomPanel, BorderLayout.SOUTH);
-    return mainFrame;
-    }
-
 }
