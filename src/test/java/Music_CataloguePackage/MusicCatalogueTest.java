ec8073eb833d9d7e7bb38ba44a08d498a4549473
samuelemmanuel211 <samuelkhamis06@gmail.com>
2025-02-01 05:43:30 -0800

    Bottom panel
    testing bottom panel is successfully
    and the result is passed

--- src/test/java/Music_CataloguePackage/MusicCatalogueTest.java
+++ src/test/java/Music_CataloguePackage/MusicCatalogueTest.java
@@ -65,6 +65,30 @@ class MusicCatalogueTest {
             assertEquals(Color.WHITE, button.getForeground(), "Buttons should have the correct text color");
         }
     }
+
+    @Test
+    void prepareBottomPanel() {
+        JFrame frame = musicCatalogue.prepareBottomPanel();
+        assertNotNull(frame, "Frame should not be null");
+        JPanel bottomPanel = (JPanel) frame.getContentPane().getComponent(2);
+        assertNotNull(bottomPanel, "Bottom panel should not be null");
+        assertEquals(new Color(217, 217, 217), bottomPanel.getBackground(), "Bottom panel should have the correct background color");
+        assertEquals(new Dimension(800, 55), bottomPanel.getPreferredSize(), "Bottom panel should have the correct size");
+
+        Component[] components = bottomPanel.getComponents();
+        assertEquals(2, components.length, "Bottom panel should contain two buttons");
+
+        JButton submitButton = (JButton) components[0];
+        JButton exitButton = (JButton) components[1];
+
+        assertEquals("Submit", submitButton.getText(), "Submit button should have correct label");
+        assertEquals(new Color(3, 60, 91), submitButton.getBackground(), "Submit button should have correct background color");
+        assertEquals(Color.WHITE, submitButton.getForeground(), "Submit button should have correct text color");
+
+        assertEquals("Exit", exitButton.getText(), "Exit button should have correct label");
+        assertEquals(new Color(3, 60, 91), exitButton.getBackground(), "Exit button should have correct background color");
+        assertEquals(Color.WHITE, exitButton.getForeground(), "Exit button should have correct text color");
+    }
 }
 
 
